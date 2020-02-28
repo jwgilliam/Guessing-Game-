92a982c94daa073ccf7e3c822cd8667575cf0510
jwgilliam <ggsmoustache@gmail.com>
2020-02-28 09:12:02 -0600

    guessing game

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GuessingGame {
+  class Program {
+    static void Main (string[] args) {
+      Random rand = new Random ();
+      int secretNumber = rand.Next (1, 11);
+      for (int i = 0; i < 3; i++) {
+
+        Console.WriteLine ("Guess a number between 1 and 10");
+        string userGuess = Console.ReadLine ();
+        int userGuessInt = int.Parse (userGuess);
+        if (userGuessInt == secretNumber) {
+          Console.WriteLine ($"You guessed it! It was {secretNumber}");
+          return;
+        } else if (userGuessInt < secretNumber && i != 2) {
+          Console.WriteLine ("Too low!");
+        } else if (userGuessInt > secretNumber && i != 2) {
+          Console.WriteLine ("Too high!");
+        } else {
+          Console.WriteLine ($"Nope, it was {secretNumber} ");
+        }
+
+      }
+    }
+  }
+
+}
